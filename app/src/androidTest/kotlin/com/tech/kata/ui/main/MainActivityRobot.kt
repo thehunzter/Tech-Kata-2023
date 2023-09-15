cc692a1d30b51f1906d20f1e8e7d31864abb2a30
duongnguyen <thanhduongit2012@gmail.com>
2023-09-15 18:24:49 +0800

    Add UI and esspresso test

--- app/src/androidTest/kotlin/com/tech/kata/ui/main/MainActivityRobot.kt
+++ app/src/androidTest/kotlin/com/tech/kata/ui/main/MainActivityRobot.kt
@@ -2,14 +2,22 @@ package com.tech.kata.ui.main
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import com.tech.kata.R
 
 class MainActivityRobot {
 
-    fun seesMainText(titleString: String) {
+    fun seesSearchButton(): MainActivityRobot {
+        onView(withId(R.id.searchButton))
+            .check(matches(isDisplayed()))
+
+        return this
+    }
+
+    fun seesMainText(titleString: String): MainActivityRobot {
         onView(withId(R.id.mainScreenText))
             .check(matches(withText(titleString)))
+
+        return this
     }
 }
--- app/src/androidTest/kotlin/com/tech/kata/ui/main/MainActivityTest.kt
+++ app/src/androidTest/kotlin/com/tech/kata/ui/main/MainActivityTest.kt
@@ -1,5 +1,20 @@
 package com.tech.kata.ui.main
 
-import org.junit.jupiter.api.Assertions.*
+import android.content.Intent
+import com.tech.kata.ui.main.base.TechKataActivityScenarioRule
+import org.junit.Test
 
-internal class MainActivityTest
+class MainActivityTest {
+
+    val mainActivity = TechKataActivityScenarioRule<MainActivity>()
+
+    val mainActivityRobot = MainActivityRobot()
+
+     @Test
+     fun onSearchClick_seesPeopleWithNameContainCharacterR() {
+         mainActivity.launchActivity(MainActivity::class.java)
+
+         mainActivityRobot.seesMainText("R2-D2")
+             .seesSearchButton()
+     }
+ }
